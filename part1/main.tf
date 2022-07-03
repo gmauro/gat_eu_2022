ec6854dccab2b71a368deecd0aacd3cf200a1992
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2022-07-03 17:39:09 +0200

    add part1

---
+++ part1/main.tf
@@ -0,0 +1,19 @@
+resource "random_id" "unique_name_suffix" {
+  byte_length = 8
+}
+
+resource "openstack_compute_keypair_v2" "my-cloud-key" {
+  name       = "tf_workshop_key_${random_id.unique_name_suffix.hex}"
+  public_key = "ssh-rsa AAAAB3NzaC1..."
+}
+
+resource "openstack_compute_instance_v2" "head" {
+  name            = "test-vm"
+  image_name      = "vggp-v40-j255-a33bb037f9fb-master"
+  flavor_name     = "c1.denbi_cloud_user_meeting"
+  key_pair        = openstack_compute_keypair_v2.my-cloud-key.name
+  security_groups = ["default"]
+  network {
+    name = "public"
+  }
+}
